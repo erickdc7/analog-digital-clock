71fae5e49f454c2df822db54effed90d234f8644
ErickDC <erickdiazc717@gmail.com>
2023-07-31 12:44:19 -0500

    variable hh & mm created

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,12 @@ const hour = document.getElementById('clock-hour'),
 const clock = () => {
     // We get the Date object
     let date = new Date()
+
+    // We get the hours and minutes 
+    // (current time) / 12(hours) * 360(deg circle)
+    // (Current minute) / 60(minutes) * 360(deg circle)
+    let hh = date.getHours() / 12 * 360,
+        mm = date.getMinutes() / 60 * 360
 }
 setInterval(clock, 1000) // (Updates every 1s) 1000 = 1s 
 
